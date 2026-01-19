7801e74348b3bbd2297f7acc63e2c26302a07495
LJS-kaist <junsoo5546@kaist.ac.kr>
2026-01-19 16:08:13 +0900

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/app/routes";
 import "@/styles/globals.css";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const GOOGLE_CLIENT_ID = "72968113031-r5q956jpo0lrhovnbv09hrpfpna2a0nm.apps.googleusercontent.com"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
