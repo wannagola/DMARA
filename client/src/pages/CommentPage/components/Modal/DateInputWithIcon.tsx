3e5cc23683a7f17d69799b3368ebfd1a5225b347
MINKI SUNG <162106492+wannagola@users.noreply.github.com>
2026-01-21 01:05:35 +0900

    calendar 위치 변경/title,date input css 수정

---
+++ client/src/pages/CommentPage/components/Modal/DateInputWithIcon.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./AddCommentModal.module.css";
+
+const DateInputWithIcon = React.forwardRef<
+  HTMLInputElement,
+  {
+    value?: string;
+    onClick?: () => void;
+    placeholder?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  }
+>(({ value, onClick, placeholder, onChange }, ref) => (
+  <div className={styles.inputWrapper} onClick={onClick}>
+    <input
+      ref={ref}
+      className={styles.input}
+      readOnly
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+    />
+    <span className={styles.calendarIcon} aria-hidden="true" />
+  </div>
+));
+
+DateInputWithIcon.displayName = "DateInputWithIcon";
+
+export default DateInputWithIcon;
